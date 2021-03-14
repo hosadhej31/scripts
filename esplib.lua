c57728e9531ca5d601c7cd7a19a00a964d811568
VoidMasterX <71610591+VoidMasterX@users.noreply.github.com>
2021-03-13 20:04:08 -0600

    Update esplib.lua

--- esplib.lua
+++ esplib.lua
@@ -13,7 +13,7 @@ EspLib.Options = {
     Health = false,
     Distance = false,
     Tracer = false,
-    Font = 0
+    Font = "UI"
 }
 
 EspLib.Services = setmetatable({}, {
@@ -89,7 +89,7 @@ local function CreateEsp(Player)
             Objects.Name.Outline = true
             Objects.Name.OutlineColor = Color3.fromRGB(0, 0, 0)
             Objects.Name.Position = Vector2.new(Head2dPosition.X, Head2dPosition.Y)
-            Objects.Name.Font = EspLib.Options.Font
+            Objects.Name.Font = Drawing,Font[EspLib.Options.Font]
     
             if (EspLib.Options.TeamColor) then
                 Objects.Name.Color = Player.Team.TeamColor.Color
